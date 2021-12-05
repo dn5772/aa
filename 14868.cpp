9e7551161b4c381133c6312f84efa2d5bb56be6e
dn5772 <shendanny@naver.com>
2021-12-06 04:29:58 +0900

    update "14868.cpp"

--- 14868.cpp
+++ 14868.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int map[2001][2001];
 int parent[100001];
+int move_x[4] = {1, -1, 0, 0};
+int mone_y[4] = {0, 0, 1, -1};
 
 struct unit{
 	int x;
@@ -17,18 +19,37 @@ int find (int x){
 	if (parent[x] == x){
 		return x;
 	} else{
-		find(parent[x])
+		find(parent[x]);
 	}
 }
+
+bool marge (int x, int y){
+	x = find(x);
+	y = find(y);
+
+	if (x == y) {return false;}
+
+	parent[x] = y;
+	return true;
+}
+
+
+
 
 
 
 int N, K;
 
-
-
 int main(){
-	cin >> N << K;
+	cin >> N >> K;
+
+	int x, y;
+
+	for (int i=1; i<=K; i++){
+		cin >> x >> y;
+		map[x][y] = i;
+		parent[i] = i;
+	}
 
 
 
