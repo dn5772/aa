07e2c24d184f1c49a75208492da0d62e6a8b71f2
dn5772 <shendanny0@gmail.com>
2021-12-06 03:45:12 +0900

    14868.cpp udate

---
+++ 14868.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+
+
+struct unit{
+	int x;
+	int y;
+};
+
+
+
+
+int N, K;
+int map[2001][2001];
+
+
+
+
+int main(){
+
+
+
+	return 0;
+}
