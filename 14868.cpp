df862f53a4ebf599f21fa5af1b03527e74247afc
dn5772 <shendanny0@gmail.com>
2021-12-06 04:01:48 +0900

    14868.cpp udate

--- 14868.cpp
+++ 14868.cpp
@@ -3,17 +3,24 @@
 
 using namespace std;
 
-
+int map[2001][2001];
+int parent[100001];
 
 struct unit{
 	int x;
 	int y;
 };
 
+queue<unit> q, q2;
+
+int find (int x){
+	if ()
+}
+
 
 
 int N, K;
-int map[2001][2001];
+
 
 
 int main(){
