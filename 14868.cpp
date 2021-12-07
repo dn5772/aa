40e1a50541cd9715405ad724deed464d9022f1ed
dn5772 <shendanny@naver.com>
2021-12-07 23:19:31 +0900

    update 14868.cpp

--- 14868.cpp
+++ 14868.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int map[2001][2001];
 int parent[100001];
 int move_x[4] = {1, -1, 0, 0};
-int mone_y[4] = {0, 0, 1, -1};
+int move_y[4] = {0, 0, 1, -1};
 
 typedef struct {
 	int x;
@@ -33,11 +33,27 @@ bool marge (int x, int y){
 	return true;
 }
 
-void bfs(){
+void bfs(){ // 문명 합치기
+	unit cur_u, u;
+
+	while (!q.empty()){
+		cur_u = q.front();
+		q.pop();
+
+		for (int i=0; i<4; i++){
+			u.x = cur_u.x + move_x[i];
+			u.y = cur_u.y + move_y[i];
+
+			
+		}
+
+		
+	}
+	
 
 }
 
-void bfs2(){
+void bfs2(){ // 문명 전파하기
 
 }
 
