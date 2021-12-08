cbac2640bdf40f588a477518a0cd07fc4406a798
dn5772 <shendanny@naver.com>
2021-12-09 00:31:37 +0900

    EAD 2789.cpp

--- 2789.cpp
+++ 2789.cpp
@@ -1,35 +1,21 @@
+#include <stdio.h>
 #include <iostream>
-// #include "List.cpp"
-// #include "String.cpp"
 
 using namespace std;
 
-
 int main(void){
-	char word[100];
 	char* cam = (char*) "CAMBRIDGE";
-	char answer[100];
-	int cnt = 0;
-	
-	cin.getline(word, 100);
 
-	// cout << word[0] << "\n" << cam[3] << endl;
-
-	for (int i=0; i<10; i++){
-		for (int j=0; j<10; j++){
-			if ((word[i] != cam[j])){
-
-				answer[cnt] = word[i];
-				cnt++;
-				cout<<cnt;
-				
+	while (1){
+		char a = getchar();
+		if (a == '\n'){
+			return 0;
+		}
+		for (int i=0; i<10; i++){
+			if (!(a == cam[i])){
+				putchar(a);
 			}
 		}
 	}
 
-	answer[cnt-1] = '\0';
-
-	cout << answer;
-
-	return 0;
 }
