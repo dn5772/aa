17424ecdbd2a2d31a0f0b8f12a87f1bf298eefd5
dn5772 <shendanny@naver.com>
2022-08-06 12:32:24 +0900

    22.8.6 12.32

---
+++ python/1040.py
@@ -0,0 +1,21 @@
+ans = ""
+n, k = input().split()
+size = len(n)
+
+if size < int(k):
+    ans += "1"
+    if int(k) < 3:
+        for i in range(int(k) - 1):
+            ans += str(i)
+    else:
+        ans += "0"
+        for i in range(2, int(k)):
+            ans += str(i)
+
+elif size == int(k):
+    ans = n
+
+else: #size > k
+
+
+print(ans)
